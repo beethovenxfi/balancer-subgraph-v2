1b53b11e550378a63bcd535e817944c386f4fab4
0xSkly <skly.beets@protonmail.com>
2022-08-24 17:48:06 +0200

    Add vault mapping for optimism

--- src/mappings/helpers/constants.ts
+++ src/mappings/helpers/constants.ts
@@ -21,6 +21,7 @@ class AddressByNetwork {
   public goerli: string;
   public polygon: string;
   public arbitrum: string;
+  public optimism: string;
   public dev: string;
 }
 
@@ -31,6 +32,7 @@ let vaultAddressByNetwork: AddressByNetwork = {
   goerli: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   polygon: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   arbitrum: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+  optimism: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   dev: '0xa0B05b20e511B1612E908dFCeE0E407E22B76028',
 };
 
@@ -43,6 +45,8 @@ function forNetwork(addressByNetwork: AddressByNetwork, network: string): Addres
     return Address.fromString(addressByNetwork.polygon);
   } else if (network == 'arbitrum-one') {
     return Address.fromString(addressByNetwork.arbitrum);
+  } else if (network == 'optimism') {
+    return Address.fromString(addressByNetwork.optimism);
   } else {
     return Address.fromString(addressByNetwork.dev);
   }
